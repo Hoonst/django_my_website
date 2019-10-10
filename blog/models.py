52a8870592a0f13f27f983ecce98b19af671f0e7
Hoonst <yoonhs1993@nate.com>
2019-10-10 18:05:24 +0900

    blog app - Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    #글 제목
+    title = models.CharField(max_length =30)
+    #글 내용
+    content = models.TextField()
+    #날짜
+    created = models.DateTimeField()
+    #작성자
+    author = models.ForeignKey(User, on_delete=True)
